ed0d344a755f719429740ca2b33136d04fa5955c
李發財 <liyanjie2048@outlook.com>
2026-07-18 16:52:05 +0800

    +

--- js.js
+++ js.js
@@ -6,8 +6,8 @@ window.onload = () => {
         },
     }).then(response => response.json()).then(data => {
         let download = document.getElementById("Download");
-        download.href = `//auto-certify.liyanjie.net/download/${data.version}.zip"`;
-        download.download = `AutoCertify.${data.version}.zip`;
-        download.querySelector('span').innerText = `立即下载 (最新版${data.version})`;
+        download.href = `//auto-certify.liyanjie.net/download/${data.Version}.zip"`;
+        download.download = `AutoCertify.${data.Version}.zip`;
+        download.querySelector('span').innerText = `立即下载 (最新版${data.Version})`;
     }).catch(error => console.log(error));
 }
